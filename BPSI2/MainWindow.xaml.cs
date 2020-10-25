b1cd7fca9f1eb4a64977a346454657795ef7383d
Tristan Games <tristanwb06@hotmail.com>
2020-10-25 15:55:03 -0700

    Checks for internet

--- BPSI2/MainWindow.xaml.cs
+++ BPSI2/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+
 
 namespace BPSI2
 {
@@ -20,9 +24,50 @@ namespace BPSI2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public bool isRunning;
         public MainWindow()
         {
             InitializeComponent();
+            StartUp();
         }
+
+        private void MediaElement_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void StartUp()
+        {
+            isRunning = true;
+            while (isRunning == true)
+            {
+                CheckForInternetConnection();
+            }
+        }
+
+        public bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    CoolKidGrid.Visibility = Visibility.Visible;
+                    return true;
+                
+            }
+            catch
+            {
+                CoolKidGrid.Visibility = Visibility.Hidden;
+                return false;
+            }
+        }
+
+        
     }
 }
